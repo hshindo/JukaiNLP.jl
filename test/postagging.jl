f7bf9ff1b16cbae01f3e121f83d63b8d43c6a35b
Hiroyuki Shindo <hshindo@outlook.com>
2016-10-15 07:46:46 +0900

    Update tagging

--- test/postagging.jl
+++ test/postagging.jl
@@ -2,33 +2,8 @@ workspace()
 using JukaiNLP
 using JukaiNLP.Tagging
 using Merlin
-using HDF5
 
-#=
-path = "C:/Users/hshindo/Dropbox/tagging3"
-vecs = open(readlines,"$(path)/nyt100.lst")
-vecs = map(vecs) do v
-    s = split(chomp(v))
-    map(x -> parse(Float32,x), s)
-end
-vec = hcat(vecs...)
-str = open(readlines,"$(path)/words.lst")
-str = map(chomp, str)
-h5open("enwords.h5", "w") do file
-    write(file, "s", str)
-    write(file, "v", vec)
-end
-throw("finish")
-=#
-
-path = joinpath(Pkg.dir("JukaiNLP"), ".corpus/postagging")
-t = Tagger("$(path)/enwords.h5")
-
-traindata = CoNLL.read("$(path)/wsj_00-18.conll", 2, 5)
-testdata = CoNLL.read("$(path)/wsj_22-24.conll", 2, 5)
-
-train(t, 10, traindata, testdata)
-throw("finish")
+train(10, traindata, testdata)
 
 modelpath = "C:/Users/hshindo/Desktop/postagger.h5"
 h5save(modelpath, Dict("wordfun"=>t.model.wordfun,"charfun"=>t.model.charfun,"sentfun"=>t.model.sentfun))
